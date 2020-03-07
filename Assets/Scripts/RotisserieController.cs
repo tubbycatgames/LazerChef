ee45984bb237b0f4d80a23b589678237e23dae23
Phil Simmons <phil@tubbycatgames.com>
2020-03-06 21:31:04 -0500

    Added Pull Support to Rotisserie and rembered to account for time

--- Assets/Scripts/RotisserieController.cs
+++ Assets/Scripts/RotisserieController.cs
@@ -7,23 +7,30 @@ public class RotisserieController : MonoBehaviour
     public float rotationSpeed = 1f;
     public float shiftSpeed = 0.5f;
     public float maxShift = 5f;
+    public float pullSpeed = 1f;
+    public float pullDepth = 3f;
+
+    private bool isPullAdjusting = false;
+    private bool isPulled = false;
 
     void Update()
     {
         UpdateRotation();
         UpdateShift();
+        UpdatePull();
     }
 
     void UpdateRotation()
     {
         var verticalInput = Input.GetAxis("Vertical");
-        transform.Rotate(0.0f, rotationSpeed * verticalInput, 0.0f);
+        var newRotation = rotationSpeed * verticalInput * Time.deltaTime;
+        transform.Rotate(0.0f, newRotation, 0.0f);
     }
 
     void UpdateShift()
     {
         var horizontalInput = Input.GetAxis("Horizontal");
-        var newX = (horizontalInput * shiftSpeed) + transform.position.x;
+        var newX = (horizontalInput * shiftSpeed * Time.deltaTime) + transform.position.x;
         if (Mathf.Abs(newX) < maxShift)
         {
             transform.position = new Vector3(
@@ -31,6 +38,43 @@ public class RotisserieController : MonoBehaviour
                 transform.position.y,
                 transform.position.z
             );
+        }
+    }
+
+    void UpdatePull()
+    {
+        if (isPullAdjusting)
+        {
+            var newZ = transform.position.z;
+            var pullDiff = pullSpeed * Time.deltaTime;
+
+            if (isPulled) 
+            {
+                newZ = Mathf.Max(newZ - pullDiff, 0f);
+            }
+            else 
+            {
+                newZ = Mathf.Min(newZ + pullDiff, maxShift);
+            }
+
+            if (newZ == 0f || newZ == maxShift) {
+                isPullAdjusting = false;
+            }
+
+            transform.position = new Vector3(
+                transform.position.x,
+                transform.position.y,
+                newZ
+            );
+
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                isPullAdjusting = true;
+                isPulled = !isPulled;
+            }
         }
     }
 }
