df61979b35c1606940f3b312a7b33ba28d71ef56
Phil Simmons <phil@tubbycatgames.com>
2020-03-06 21:07:28 -0500

    Updated Unity Version and added basic Rotisserie Controller

---
+++ Assets/Scripts/RotisserieController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotisserieController : MonoBehaviour
+{
+    public float rotationSpeed = 1f;
+    public float shiftSpeed = 0.5f;
+    public float maxShift = 5f;
+
+    void Update()
+    {
+        UpdateRotation();
+        UpdateShift();
+    }
+
+    void UpdateRotation()
+    {
+        var verticalInput = Input.GetAxis("Vertical");
+        transform.Rotate(0.0f, rotationSpeed * verticalInput, 0.0f);
+    }
+
+    void UpdateShift()
+    {
+        var horizontalInput = Input.GetAxis("Horizontal");
+        var newX = (horizontalInput * shiftSpeed) + transform.position.x;
+        if (Mathf.Abs(newX) < maxShift)
+        {
+            transform.position = new Vector3(
+                newX,
+                transform.position.y,
+                transform.position.z
+            );
+        }
+    }
+}
