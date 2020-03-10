afd2da1da5dff547aa548bd51ba107d65db83e1d
Phil Simmons <phil@tubbycatgames.com>
2020-03-10 10:22:11 -0400

    Added Basic Lazer Support and a Lazer testing playground

---
+++ Assets/Scripts/Lazer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lazer : MonoBehaviour
+{
+    public float maxDistance = 5000f;
+    private LineRenderer beam;
+
+    void Start()
+    {
+        beam = GetComponent<LineRenderer>();
+        beam.SetPosition(0, transform.position);
+    }
+
+    void Update()
+    {
+        var up = transform.up;
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, up, out hit) && hit.collider)
+        {
+            beam.SetPosition(1, hit.point);
+        }
+        else
+        {
+            beam.SetPosition(1, up * maxDistance);
+        }
+    }
+}
---
+++ Assets/Scripts/LazerTestTarget.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LazerTestTarget : MonoBehaviour
+{
+    public Lazer lazer;
+    public float height = 3f;
+    public float jump = 2f;
+
+    void Start()
+    {
+        transform.position = new Vector3(
+            lazer.transform.position.x,
+            lazer.transform.position.y + height,
+            lazer.transform.position.z
+        );
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            var lazerX = lazer.transform.position.x;
+            var newX = transform.position.x == lazerX
+                ? lazerX + jump
+                : lazerX;
+            transform.position = new Vector3(
+                newX,
+                transform.position.y,
+                transform.position.z
+            );
+        }
+    }
+}
